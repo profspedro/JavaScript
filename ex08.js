6b6cd3a2064ca1a17cb86bbd2a68599ed593669f
profspedro <spedrodf@gmail.com>
2024-08-03 18:06:20 -0300

    subindo exerc. de 08 a 10

---
+++ ex08.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.logd('A velocidade do seu carro é ${vel}km/h')
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija com cuidado! Use sempre o cinto de segurança.')
